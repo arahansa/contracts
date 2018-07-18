7173142c7586662bbb6d358434b5ba5bc25c3ba3
lucas.choi <lucas.choi@mcngamehub.com>
2018-07-18 12:16:44 +0900

    update PlayCoinSafeMath function identifier : view -> pure

--- contracts/PlayCoinSafeMath.sol
+++ contracts/PlayCoinSafeMath.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.24;
 
 /**
- * @title SafeMath
+ * @title PlayCoinSafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library PlayCoinSafeMath {
 
-    function sub(uint8 a, uint8 b) internal view returns (uint8) {
+    function sub(uint8 a, uint8 b) internal pure returns (uint8) {
         assert(b <= a);
         return a - b;
     }
